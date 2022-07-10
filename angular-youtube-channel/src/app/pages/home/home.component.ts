4c1f881014056ffd26d8633a8b5cbb20b30e20be
yporras <yuzmaporras@falabella.cl>
2022-07-10 18:08:30 -0400

    added call to youtube

--- angular-youtube-channel/src/app/pages/home/home.component.ts
+++ angular-youtube-channel/src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Video } from 'src/app/models/youtue.models';
+import { YoutubeService } from 'src/app/services/youtube.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  videos: Video[] = [];
 
-  constructor() { }
+  constructor(private youtubeService: YoutubeService) { }
 
   ngOnInit(): void {
+
+    this.youtubeService.getVideos()
+    .subscribe(resp => {
+     
+      this.videos.push(...resp)
+      console.log('resṕ', this.videos);
+    })
   }
 
 }
--- angular-youtube-channel/src/app/services/youtube.service.ts
+++ angular-youtube-channel/src/app/services/youtube.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { YoutubeResponse } from '../models/youtue.models';
+import { map } from 'rxjs/operators'
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
 
-  constructor() { }
+  private youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+  private apiKey     = 'AIzaSyCls9BAkHxG3PuecD7YHN1Srn1y6zUwHEM'
+  private playlist   = 'UUuaPTYj15JSkETGnEseaFFg';
+  private nextPageToken = '';
+
+  constructor(private http: HttpClient) { 
+
+    
+  }
+
+  getVideos(){
+    const url = `${this.youtubeUrl}/playlistItems`;
+    const params = new HttpParams()
+    .set('part', 'snippet')
+    .set('maxResults', '10')
+    .set('playlistId', this.playlist)
+    .set('key', this.apiKey)
+
+    return this.http.get<YoutubeResponse>(url, { params })
+            .pipe(
+              map(resp => {
+                this.nextPageToken = resp.nextPageToken;
+                return resp.items;
+              }),
+              map(items => items.map(video => video.snippet))
+              )
+  }
 }
