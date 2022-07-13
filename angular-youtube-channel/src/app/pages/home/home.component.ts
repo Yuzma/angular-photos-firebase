d72608b537473bfa99a6f3a1057cfa377e0546bf
yporras <yuzmaporras@falabella.cl>
2022-07-12 20:06:17 -0400

    new animated

--- angular-youtube-channel/src/app/pages/home/home.component.ts
+++ angular-youtube-channel/src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Video } from 'src/app/models/youtue.models';
 import { YoutubeService } from 'src/app/services/youtube.service';
-
+import Swal from 'sweetalert2'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,13 +13,27 @@ export class HomeComponent implements OnInit {
   constructor(private youtubeService: YoutubeService) { }
 
   ngOnInit(): void {
+this.uploadVideos();
+    
+  }
 
+  uploadVideos(){
     this.youtubeService.getVideos()
     .subscribe(resp => {
      
       this.videos.push(...resp)
       console.log('resṕ', this.videos);
     })
+  }
+
+  showVideo (video:Video){
+    console.log(video);
+    Swal.fire({
+      html: `
+      <h4>${ video.title }</h4>
+      <iframe width="100%" height="315" src="https://www.youtube.com/embed/${video.resourceId.videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+    })
+    
   }
 
 }
